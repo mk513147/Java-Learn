5719ddfd6c58b18d20076f8e1ba9734ac79e4533
Mohit kumar <mk513147@gmail.com>
2025-10-23 13:20:04 +0530

    UPDATE Added Cyclic Sort

--- Assignments/Arrayss/Sorting.java
+++ Assignments/Arrayss/Sorting.java
@@ -3,11 +3,40 @@ import java.util.Arrays;
 public class Sorting {
     public static void main(String[] args) {
         int[] nums = { 5, 3, 1, 4, 6, 2 };
-        // bubbleSort(nums);
-        // selectionSort(nums);
-        insertitionSort(nums);
-        System.out.println(Arrays.toString(nums));
 
+        // Make copies of the original array for fair comparison
+        int[] arr1 = Arrays.copyOf(nums, nums.length);
+        int[] arr2 = Arrays.copyOf(nums, nums.length);
+        int[] arr3 = Arrays.copyOf(nums, nums.length);
+        int[] arr4 = Arrays.copyOf(nums, nums.length);
+
+        // Bubble Sort
+        long start = System.nanoTime();
+        bubbleSort(arr1);
+        long end = System.nanoTime();
+        System.out.println("Bubble Sort: " + Arrays.toString(arr1));
+        System.out.println("Time taken (ns): " + (end - start));
+
+        // Selection Sort
+        start = System.nanoTime();
+        selectionSort(arr2);
+        end = System.nanoTime();
+        System.out.println("\nSelection Sort: " + Arrays.toString(arr2));
+        System.out.println("Time taken (ns): " + (end - start));
+
+        // Insertion Sort
+        start = System.nanoTime();
+        insertitionSort(arr3);
+        end = System.nanoTime();
+        System.out.println("\nInsertion Sort: " + Arrays.toString(arr3));
+        System.out.println("Time taken (ns): " + (end - start));
+
+        // Cyclic Sort
+        start = System.nanoTime();
+        cyclicSort(arr4);
+        end = System.nanoTime();
+        System.out.println("\nCyclic Sort: " + Arrays.toString(arr4));
+        System.out.println("Time taken (ns): " + (end - start));
     }
 
     static void swap(int[] arr, int num1, int num2) {
@@ -30,16 +59,14 @@ public class Sorting {
         boolean swapped;
         for (int i = 0; i < nums.length; i++) {
             swapped = false;
-            for (int j = 1; j < nums.length - 1; j++) {
+            for (int j = 1; j < nums.length - i; j++) {
                 if (nums[j] < nums[j - 1]) {
-                    swap(nums, i, j);
+                    swap(nums, j, j - 1);
                     swapped = true;
                 }
             }
-            if (!swapped) {
+            if (!swapped)
                 break;
-            }
-
         }
     }
 
@@ -59,6 +86,18 @@ public class Sorting {
                 } else {
                     break;
                 }
+            }
+        }
+    }
+
+    static void cyclicSort(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
             }
         }
     }
