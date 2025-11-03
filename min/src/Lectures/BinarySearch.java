cc00c876c72d3fc109c2d5ee6f342ba99c28f850
Mohit kumar <mk513147@gmail.com>
2025-11-03 14:18:21 +0530

    UPDATE Changed main method to use recursiveBinarySearch instead of binarySearch for improved functionality

--- min/src/Lectures/BinarySearch.java
+++ min/src/Lectures/BinarySearch.java
@@ -2,7 +2,7 @@ public class BinarySearch {
     public static void main(String args[]) {
         int[] arr = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int target = 6;
-        System.out.println("Element found at index: " + binarySearch(arr, target));
+        System.out.println("Element found at index: " + recursiveBinarySearch(arr, target, 0, arr.length - 1));
     }
 
     static int binarySearch(int[] arr, int target) {
@@ -25,5 +25,21 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    static int recursiveBinarySearch(int[] arr, int target, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+        if (target > arr[mid]) {
+            return recursiveBinarySearch(arr, target, mid + 1, end);
+        }
+        return recursiveBinarySearch(arr, target, start, mid - 1);
     }
 }
